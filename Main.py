ff6c79df22a1dac5e423f076ca96ad4de17b9e68
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2023-10-24 00:07:03 -0500

    Added new types of graphs

--- Main.py
+++ Main.py
@@ -4,34 +4,46 @@ import matplotlib.pyplot as plt
 # Load data from the CSV file
 df = pd.read_csv('customer_data.csv')
 
-# Print the available column names so the user can choose
+# Output the names of the columns so user can choose which to put on each axis
 print("Available columns:")
 for col in df.columns:
     print(col)
 
-# Prompt the user to enter the column names for x and y
+# Have the user input the axis' and type of graph they want
 x_column = input("Enter the column name for the x-axis: ")
 y_column = input("Enter the column name for the y-axis: ")
+plot_type = input("Enter the plot type (bar, pie, line): ")
 
-# Create a bar graph
+# Create a graph based on the user's choice
 if x_column in df.columns and y_column in df.columns:
     data = df[[x_column, y_column]]
-    data_counts = data.groupby(x_column).count()
 
-    # Create a custom sorting order for IncomeRange
-    if(x_column == 'IncomeRange'):
-        custom_order = sorted(data_counts.index, key=lambda x: int(x.split('-')[0]))
-        data_counts = data_counts.reindex(custom_order)
-    else:
+    if plot_type == "bar":
         data_counts = data.groupby(x_column).count()
 
-    data_counts.plot(kind='bar')
+        # Increasing order for IncomeRange and YearPurchased
+        if x_column in ['IncomeRange', 'YearPurchased']:
+            custom_order = sorted(data_counts.index, key=lambda x: int(x.split('-')[0]))
+            data_counts = data_counts.reindex(custom_order)
 
-    # Customize the graph
+        data_counts.plot(kind='bar')
+
+    elif plot_type == "pie":
+        data_pie = data.groupby(x_column).size()
+        data_pie.plot(kind='pie', autopct='%1.1f%%')
+
+    elif plot_type == "line":
+        data_line = data.groupby(x_column).sum()
+        data_line.plot(kind='line', marker='o')
+
+    # Customizes the graph for asthetics
     plt.title(f'Number of {y_column} by {x_column}')
     plt.xlabel(x_column)
     plt.ylabel(f'Number of {y_column}')
     plt.xticks(rotation=45, fontsize=8)
+
     plt.show()
+
+#If the user input is invalid 
 else:
-    print("Invalid column names. Please check the column names in your data.")
+    print("Invalid input. Please check the column names in your data.")
