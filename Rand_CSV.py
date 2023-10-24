d77a2205af08092bdc24b91e1f539f2ec3e98ed6
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2023-10-24 00:07:42 -0500

    Added dates of purchase

--- Rand_CSV.py
+++ Rand_CSV.py
@@ -14,18 +14,27 @@ income_ranges = [
     "100000-1000000",
 ]
 
+date_ranges = [
+    "2013-2015",
+    "2015-2017",
+    "2017-2019",
+    "2019-2021",
+    "2021-2023",
+]
+
 # Generate 100 customers
 for customer_id in range(1, 101):
     age = random.randint(18, 70)
     gender = random.choice(['Male', 'Female'])
     purchases = random.randint(0, 10)
     income_range = random.choice(income_ranges)  # Randomly select an income range
+    date_range = random.choice(date_ranges) # Random date range
 
-    data.append([customer_id, age, gender, purchases, income_range])
+    data.append([customer_id, age, gender, purchases, income_range, date_range])
 
 # Create a DataFrame
-columns = ['CustomerID', 'Age', 'Gender', 'Purchases', 'IncomeRange']
+columns = ['CustomerID', 'Age', 'Gender', 'Purchases', 'IncomeRange', 'YearPurchased']
 df = pd.DataFrame(data, columns=columns)
 
-# Save the DataFrame as a CSV file
+# Save the DataFrame as the CSV file
 df.to_csv('customer_data.csv', index=False)
